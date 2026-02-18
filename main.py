69cefd8cb1d3db9d7869cb684f4c663df8d9d8d0
abhaysinghal126 <abhaysinghal126@gmail.com>
2026-02-18 18:10:49 +0200

    added a feature to make different universes and compare them with eachother

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def is_success(probability: float) -> bool:
 
 def simulator(probability: float, reward: float, penalty: float, number_of_simulations: int) -> dict:
 
-    if not type(number_of_simulations) == int:
+    if not isinstance(number_of_simulations, int):
         raise ValueError("The type of the number of simulations must be an integer")
     if not 0 < number_of_simulations:
         raise ValueError("The number of simulations must be greater than 0.")
@@ -39,8 +39,37 @@ def simulator(probability: float, reward: float, penalty: float, number_of_simul
         "rate_of_profitability": rate_of_profitability
     }
 
+def run_universe(probability: float, reward: float, penalty: float, simulations_per_universe: int) -> int:
+    balance = 0
+
+    for _ in range(simulations_per_universe):
+        if(is_success(probability)):
+            balance += reward
+        else:
+            balance += penalty
+
+    return balance
+
+def universe_simulator(probability: float, reward: float, penalty: float, number_of_universes: int,simulations_per_universe: int) -> dict:
+    final_balances = []
+    balance = 0
+
+    for _ in range (number_of_universes):
+        balance = run_universe(probability, reward, penalty, simulations_per_universe)
+        final_balances.append(balance)
+
+    average_final_balance = sum(final_balances) / number_of_universes
+    profitable_universes = sum(1 for b in final_balances if b > 0)
+
+    return {
+        "average_final_balance": average_final_balance,
+        "probability_of_profit": profitable_universes / number_of_universes,
+        "best_universe": max(final_balances),
+        "worst_universe": min(final_balances)
+    }
+
 if __name__ == "__main__":
-    result = simulator(0.3, 100, -40, 1000000)
+    result = universe_simulator(0.95, 0.80, -80, 1000, 1000)
     print(result)
 
 
